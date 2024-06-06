ea85ef7d36ca336469f9a3a32a2f28c23537e60d
Oleksandr <ikievite@gmail.com>
2024-06-06 10:32:00 +0300

    updated `tmux` script

--- start_tmux_session.sh
+++ start_tmux_session.sh
@@ -2,12 +2,7 @@
 
 session_name=session
 
-venv=~/.venv/bin
-
-cd ~/git/MiscScripts
 tmux new-session -s "$session_name" -n window1 -d
-tmux send-keys -t "$session_name" "cd scripts" Enter
-tmux send-keys -t "$session_name" "source $venv/activate" Enter
 tmux send-keys -t "$session_name" "ipython" Enter
 
 tmux split-window -h -t "$session_name"
@@ -20,5 +15,5 @@ tmux split-window -v -t "$session_name"
 
 tmux select-pane -t 0
 
-tmux select-window -t "$session_name:1"
 tmux -2 attach-session -t "$session_name"
+
